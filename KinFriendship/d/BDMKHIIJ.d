8b523722b48550896269b332bef26bca189cb44c
bucketfulofsunshine <135817175+bucketfulofsunshine@users.noreply.github.com>
2024-10-13 22:43:55 +0200

    Add files via upload

--- KinFriendship/d/BDMKHIIJ.d
+++ KinFriendship/d/BDMKHIIJ.d
@@ -1,7 +1,12 @@
+// Interjections 
+I_C_T BDZIATAR 0 BFSMk.Ziatar
+== BDMKHIIJ IF ~InParty("MKHIIN") InMyArea("MKHIIN") !StateCheck("MKHIIN",CD_STATE_NOTVALID)~ THEN ~Simple. Walked right in.~
+END
+
 APPEND BDMKHIIJ
 
 // Talk 1- she appreciates that you helped her, but it was still a little sus of you
-IF WEIGHT #-1 ~Global("BFSKinFriendshipTalks","GLOBAL",2)~ THEN BEGIN BFSMk.Talk1
+IF WEIGHT #-2 ~Global("BFSKinFriendshipTalks","GLOBAL",2)~ THEN BEGIN BFSMk.Talk1
 SAY ~Why'd you help me? Goblins don't get kindness for free.~
 ++ ~It felt like the right thing to do.~ + BFSMk.Right
 ++ ~I don't believe in enslaving anyone.~ + BFSMk.Slavery
@@ -50,7 +55,7 @@ IF ~~ + BFSMk.WorldNorm
 END
 
 IF ~~ BFSMk.Shutup
-SAY ~Didn't think you were. Was trying t'figure out what you *did* want.~
+SAY ~Didn't think you were. Was trying to figure out what you *did* want.~
 IF ~~ DO ~SetGlobal("BFSKinFriendshipTalks","GLOBAL",3) RealSetGlobalTimer("BFSKinFriendshipTalksTimer","GLOBAL",3600)~ EXIT
 END
 
@@ -63,12 +68,12 @@ SAY ~That's how it works. The strong take from the weak. From those smaller. Mak
 END
 
 IF ~~ BFSMk.HelpBefore
-SAY ~Maybe. Still hard t'believe.~
+SAY ~Maybe. Still hard to believe.~
 IF ~~ + BFSMk.Cont
 END
 
 IF ~~ BFSMk.EarnTrust
-SAY ~Would be nice t'be wrong. Guess we'll see. I'm not going anywhere.~
+SAY ~Would be nice to be wrong. Guess we'll see. I'm not going anywhere.~
 IF ~~ + BFSMk.Cont
 END
 
@@ -79,7 +84,7 @@ END
 
 IF ~~ BFSMk.Cont
 SAY ~Haven't been around others much. Not since leaving the tribe.~
-= ~The drow's hospitality doesn't count. Being prodded with sticks and all.~
+= ~The drow's hospitality don't count. Being prodded with sticks and all.~
 ++ ~Relax, M'khiin, I don't have any expectations.~ + BFSMk.Cont2
 + ~Class(Player1,BARD)~ + ~Then it's a good thing I talk enough for both of us.~ + BFSMk.Cont2
 + ~!Class(Player1,SHAMAN)~ + ~But you're a shaman. You talk to spirits all the time, don't you?~ + BFSMk.ShamanConverse
@@ -98,7 +103,7 @@ IF ~~ DO ~SetGlobal("BFSKinFriendshipTalks","GLOBAL",3) RealSetGlobalTimer("BFSK
 END
 
 // Talk 2 - A matter of food / resources and fitting in
-IF WEIGHT #-2 ~Global("BFSKinFriendshipTalks","GLOBAL",4)~ THEN BEGIN BFSMk.Talk2
+IF WEIGHT #-1 ~Global("BFSKinFriendshipTalks","GLOBAL",4)~ THEN BEGIN BFSMk.Talk2
 SAY ~Been thinking. How much food can I take?~  
 ++ ~Why are we even having this conversation?~ + BFSMk.WhyDiscussFood
 + ~InParty("Minsc")~ + ~As a member of this group, you receive an equal share of every meal.~ + BFSMk.EqualFoodM
@@ -131,13 +136,23 @@ IF ~~ + BFSMk.FoodTell
 END
 
 IF ~~ BFSMk.FoodTell
-SAY ~What'd'you like t'eat?~
+// Word this line better OMG
+SAY ~What food do you prefer?~
 ++ ~I can't pick a favourite.~ + BFSMk.NoFave
 ++ ~Roasted chicken. You can't go wrong with a classic.~ + BFSMk.Chicken
 ++ ~I prefer a hearty stew. Simple but delicious.~ + BFSMk.StewYum
 ++ ~Stonebread. It lasts even when the road's rough.~ + BFSMk.Yum
 ++ ~Starfruit tart. It's delicate and flaky with just a hint of sweetness.~ + BFSMk.Yum
 ++ ~I'm fond of savoury rice. Anything with a bit of spice really.~ + BFSMk.Yum
++ ~Class(Player1,DRUID)~ + ~Foraged meals. Nature provides whatever I need.~ + BFSMk.ClassCircleBack
++ ~Class(Player1,MONK)~ + ~I prefer simple meals. Food should nourish the body and mind.~ + BFSMk.ClassCircleBack
+END
+
+IF ~~ BFSMk.ClassCircleBack
+SAY ~If we come by something good, I'll leave more for you.~
+++ ~No. You don't have to go hungry on my account.~ +  BFSMk.MealTalkEnd
+++ ~Fair enough. If we come across anything you like, tell me and I'll return the favor.~ + BFSMk.MealTalkEnd
+++ ~Sounds like a plan.~ + BFSMk.MealTalkEnd
 END
 
 IF ~~ BFSMk.NoFave
@@ -157,7 +172,7 @@ SAY ~Like it too.~
 END
 
 IF ~~ BFSMk.StewYum
-SAY ~Stew's not bad. Learned t'make some after leaving the tribe.~
+SAY ~Stew's not bad. Learned to make some after leaving the tribe.~
 = ~If we come by it, I'll leave more for you.~ 
 ++ ~No. You don't have to go hungry on my account.~ +  BFSMk.MealTalkEnd
 ++ ~Fair enough. If we come across anything you like, tell me and I'll return the favor.~ + BFSMk.MealTalkEnd
@@ -174,7 +189,7 @@ END
 IF ~~ BFSMk.WhyDiscussFood
 SAY ~Tribe fought over food. The fastest or strongest ate first. Everyone else got what's left.~
 = ~Goblins settle for anything. Have to. We don't farm. Don't grow. We steal, we scrounge, we dig through what others throw away. Scraps of scraps.~ 
-= ~Need t'know where I stand. What's my share?~
+= ~Need to know where I stand. What's my share?~
 + ~InParty("Minsc")~ + ~As a member of this group, you receive an equal share of every meal.~ + BFSMk.EqualFoodM
 + ~!InParty("Minsc")~ + ~As a member of this group, you receive an equal share of every meal.~ + BFSMk.EqualFood
 + ~InParty("Minsc")~ + ~Goblins are small. You don't need much.~ + BFSMk.FoodSmallM
@@ -193,48 +208,156 @@ SAY ~I like that. Wouldn't work in a goblin tribe though.~
 IF ~~ DO ~SetGlobal("BFSKinFriendshipTalks","GLOBAL",5) RealSetGlobalTimer("BFSKinFriendshipTalksTimer","GLOBAL",3600)~ EXIT
 END
 
-// PID 
+// Scenery Talks
+// Dragon Cave Warning
+IF ~Global("BFSMkDragon","GLOBAL",1)~ BFSMk.DragonWarning
+SAY ~Spirits stir. Something's up ahead.~
+++ ~It's nothing we can't handle.~ + BFSMk.DragonThanks
+++ ~I appreciate the warning, but we've no choice but to press ahead.~ + BFSMk.DragonThanks
+++ ~We'll tread lightly. Keep your voice low.~ + BFSMk.DragonQuiet
+++ ~The snoring gave that away.~ + BFSMk.DragonThanks
++ ~Class(Player1,SHAMAN)~ + ~I hear them too. We should be cautious.~ + BFSMk.DragonThanks
+END
+
+IF ~~ BFSMk.DragonThanks
+SAY ~Right. I'll be here.~ 
+IF ~~ DO ~SetGlobal("BFSMkDragon","GLOBAL",2)~ EXIT
+END
+
+IF ~~ BFSMk.DragonQuiet
+SAY ~Won't matter. Not with all the snoring.~ 
+IF ~~ DO ~SetGlobal("BFSMkDragon","GLOBAL",2)~ EXIT
+END
+
+// Dragon Dead
+IF ~Global("BFSMkDragon","GLOBAL",3)~ BFSMK.DragonDead
+SAY ~Dead now. Spirits are quieter.~
+IF ~~ DO ~SetGlobal("BFSMkDragon","GLOBAL",4)~ EXIT
+END
+
+// Post Boareskyr Bridge, she's *trying* 
+IF ~Global("BFSMkBoareskyr","GLOBAL",3)~ THEN BEGIN BFSMk.Boareskyr
+SAY ~Hmph. You help 'em, something bad happens, now they don't like you anymore. Been the same for me. One look and the worst thoughts start crawling in. Don't blame 'em. Haven't had good experiences with goblins either.~
+= ~Just a scorch mark on the ground. You're still the same <CHARNAME>.~
+++ ~M'Khiin, are you trying to comfort me?~ + BFSMk.Console
+++ ~Being judged for *what* you are, rather than who you are, is the worst.~ + BFSMk.Console
+++ ~I don't need your sympathy, M'Khiin. Back off..~ + BFSMk.NoScrubs
+++ ~Don't even think about comparing yourself to me.~ + BFSMk.NoComparison
+END
+
+IF ~~ BFSMk.Console
+SAY ~Maybe I'm being sensible. Lots of the too-talls aren't.~
+= ~But if you feel better. Yes. That was my intent.~ 
+++ ~Thank you. I appreciate it.~ + BFSMk.Kind
+++ ~Oddly enough that does make me feel better.~ + BFSMk.Kind
+++ ~There's more going on here than you realize, M'Khiin. Different faiths. Different cities.~ + BFSMk.Overcomplicated
+++ ~They don't. So leave me alone.~ + BFSMk.NoScrubs
+END
+
+IF ~~ BFSMk.Overcomplicated
+SAY ~Too much noise for one goblin's head. Could make it simpler. But don't think you will.~
+IF ~~ + BFSMk.Kind
+END
+
+IF ~~ BFSMk.Kind
+SAY ~I'll brew you a stew. Mushroom and moss. Good for the soul. I'm a shaman, I'd know.~
+++ ~I'll pass.~ + BFSMk.NoSoup
+++ ~I appreciate the thought, but it would be rude not to share with the whole army. We'll have to wait.~ + BFSMk.NoSoup
+++ ~That sounds delicious! Count me in!~ + BFSMk.YesSoup
+++ ~No, M'Khiin. Just…no.~ + BFSMk.NoSoup
++ ~Class(Player1,DRUID)~ + ~I'd love to trade remedies with you. It's not often I can talk with a Goblin Shaman.~ + BFSMk.DruidStew
+END
+
+IF ~~ BFSMk.NoSoup
+SAY ~More for me then.~ 
+IF ~~ THEN DO ~SetGlobal("BFSMkBoareskyr","GLOBAL",4)~ EXIT
+END
+
+IF ~~ BFSMk.YesSoup
+SAY ~Might be looking forward to it .~
+IF ~~ THEN DO ~SetGlobal("BFSMkBoareskyr","GLOBAL",4)~ EXIT
+END
+
+IF ~~ BFSMk.DruidStew
+SAY ~Not often you'd want to. Wouldn't mind sharing though. Not with you.~
+IF ~~ THEN DO ~SetGlobal("BFSMkBoareskyr","GLOBAL",4)~ EXIT
+END
+
+IF ~~ BFSMk.NoScrubs
+SAY ~I know about being unwanted. Or being were you don't want to be.~
+IF ~~ THEN DO ~SetGlobal("BFSMkBoareskyr","GLOBAL",4)~ EXIT
+END
+
+IF ~~ BFSMk.NoComparison
+SAY ~Why not? Seems similar. People think you're a monster. Maybe you are, maybe you're not. Doesn't matter. They already made up their minds.~ 
+= ~You're still you though.~ 
+IF ~~ THEN DO ~SetGlobal("BFSMkBoareskyr","GLOBAL",4)~ EXIT
+END
+
+// Player Initiated Dialogue (PID)
 IF ~IsGabber(Player1)~ BFSMk.PID    			
-SAY ~Something t'say?~ 
-++ ~I have a few questions.~ + BFSMk.QuestionHub
+SAY ~Something to say?~ 
++ ~RandomNum(4,1)~ + ~How are you doing?~ + BFSMk.HowU1
++ ~RandomNum(4,2)~ + ~How are you doing?~ + BFSMk.HowU2
++ ~RandomNum(4,3)~ + ~How are you doing?~ + BFSMk.HowU3
++ ~HPPercentLT("MKHIIN",80) RandomNum(4,4)~ + ~How are you doing?~ + BFSMk.HowU4
++ ~Global("BFSKinShamanPID","GLOBAL",0)~ + ~How did you learn to speak with spirits?~ + BFSMk.ISeeDeadPeople
++ ~Global("BFSKinTribePID","GLOBAL",0)~ + ~Why did you leave your tribe?~ + BFSMk.MkhiinSolo
++ ~Global("BFSKinFearPID","GLOBAL",0)~ + ~Is there anything you fear?~ + BFSMk.FearIsTheMindKiller
++ ~Global("BFSKinSpellPID","GLOBAL",0)~ + ~What's your favourite spell?~ + BFSMk.MagicHands
++ ~GlobalGT("BFSKinFriendshipTalks","GLOBAL",4) Global("BFSKinFaveFoodKin","GLOBAL",0)~ + ~I never got around to asking you. What's your favorite thing to eat?~ + BFSMk.OmNomKin
 ++ ~Let's keep moving.~ + BFSMk.PIDEnd
 END
 
+IF ~~ BFSMk.HowU1
+SAY ~Surviving.~ 
+IF ~~ EXIT
+END
+
+IF ~~ BFSMk.HowU2
+SAY ~Could be better. Could be worse.~ 
+IF ~~ EXIT
+END
+
+IF ~~ BFSMk.HowU3
+SAY ~Couldn't be better!~ 
+IF ~~ EXIT
+END
+
+IF ~~ BFSMk.HowU4
+SAY ~Still standing. But barely.~ 
+IF ~~ EXIT
+END
+
 IF ~~ BFSMk.PIDEnd
-SAY ~Good. We've wasted enough time.~ 
+SAY ~Ready for whatever comes next.~ 
 IF ~~ EXIT
 END
 
-IF ~~ BFSMk.QuestionHub
-SAY ~Ask then.~ 
-++ ~Do you miss being around your kind?~ + BFSMk.MissHome
-++ ~How did you learn to speak with spirits?~ + BFSMk.SpiritOrigin
-// ++ ~Why do goblins live the way they do?~ + BFSMk.GoblinCulture // because Maglubiyet (check spelling) and always being pushed around
-++ ~I have no further questions.~ + BFSMk.PIDEnd
+IF ~~ BFSMk.OmNomKin
+SAY ~Anything that doesn't try to eat me first.~ 
+IF ~~ THEN DO ~SetGlobal("BFSKinSpellPID","GLOBAL",1)~ EXIT
 END
 
-IF ~~ BFSMk.SpiritOrigin
+IF ~~ BFSMk.MagicHands
+SAY ~Whatever keeps me alive the longest.~
+IF ~~ THEN DO ~SetGlobal("BFSKinFaveFoodKin","GLOBAL",1)~ EXIT
+IF ~~ EXIT
+END
+
+IF ~~ BFSMk.ISeeDeadPeople
 SAY ~Didn't learn. Not like that. Words don't matter. You gotta listen.~
-// expand here
-IF ~~ EXIT
+IF ~~ THEN DO ~SetGlobal("BFSKinShamanPID","GLOBAL",1)~ EXIT
 END
 
-IF ~~ BFSMk.MissHome
-SAY ~No. Here is better.~
-++ ~Care to elaborate on that?~ + BFSMk.FixThings
-++ ~Well, I'm glad you're happy.~ + BFSMk.HappyNotWithGoblins
-++ ~Wouldn't you rather fix things with your tribe, instead of wandering with strangers?~ + BFSMk.FixThings
+IF ~~ BFSMk.MkhiinSolo
+SAY ~Didn't fit. Simple as that.~
+IF ~~ THEN DO ~SetGlobal("BFSKinTribePID","GLOBAL",1)~ EXIT
 END
 
-IF ~~ BFSMk.HappyNotWithGoblins
-SAY ~Got a full belly. A safe spot t'sleep. Had worse.~  
-IF ~~ EXIT
-END
-
-IF ~~ BFSMk.FixThings
-SAY ~Better t'talk t'strangers. Tribe heard me, didn't understand, didn't want to.~
-= ~So I left.~
-IF ~~ EXIT
+IF ~~ BFSMk.FearIsTheMindKiller
+SAY ~Nothing worth sharing. Fear gets you killed.~
+IF ~~ THEN DO ~SetGlobal("BFSKinFearPID","GLOBAL",1)~ EXIT
 END
 
 END // FOR APPEND
